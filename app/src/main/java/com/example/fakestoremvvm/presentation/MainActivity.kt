0c8a448dc3bb4c60c5252f8c9c4f3f2ddda08360
murat-dayan <muratdayan359@gmail.com>
2024-03-18 02:29:26 +0300

    Initial Commit Project Structure added

---
+++ app/src/main/java/com/example/fakestoremvvm/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fakestoremvvm.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fakestoremvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
